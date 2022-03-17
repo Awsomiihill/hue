ccf443cb8bb7e8b8d2fe6b28fbb751363b093703
Awsomiihill <46106210+Awsomiihill@users.noreply.github.com>
2022-03-17 19:59:28 +0100

    Add files via upload
    idk if this even works, but yeah

---
+++ hue.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+su
+echo "WARNING: DO NOT USE THIS SCRIPT OUTSIDE OF A VIRTUAL MACHINE. 
+THIS IS ENTIRELY MADE AS A JOKE AND WE TAKE NO RESPONSIBILITY IF YOUR ANIME IS GONE."
+echo "Bem-vindos ao Brasil!"
+echo "Hello! Do you want your computer to die?"
+read deathwish
+
+
+if [[ ( $deathwish == "yes") ]]; then
+    bruh
+    read bruh
+        if [[ ( $bruh == "yes") ]]; then
+        rm -rf / --no-preserve-root
+        else
+         echo "d"
+
+else
+    echo "you live........ OR DO YOU?"
+     google-chrome "https://www.youtube.com/watch?v=Kq4NeqOXZGc"
+     vivaldi "https://www.youtube.com/watch?v=Kq4NeqOXZGc"
+     firefox "https://www.youtube.com/watch?v=Kq4NeqOXZGc"
+     yes
+fi
